6859d6577a0ee2d0cb96338a59bc220121cd5962
Жикин Сергей <sergei.zhikin@domru.ru>
2020-01-20 13:19:03 +0500

    added success login test and fail login test

--- src/test/java/ru/greenData/FirstTest.java
+++ src/test/java/ru/greenData/FirstTest.java
@@ -15,6 +15,12 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class FirstTest extends WebDriverSettings {
+    String login="tester";
+    String password="K35G3U";
+    String fakeLogin="test";
+    String fakePassword="1";
+    String emptyPassword="";
+    String emptyLogin="";
 
 
     @Test
@@ -26,4 +32,21 @@ public class FirstTest extends WebDriverSettings {
         loginPage.getStarted();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("error")));
     }
+    @Test
+    public void errorAuthTest() {
+        LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+        loginPage.open();
+        loginPage.loginWithLoginButtonCurrent(emptyLogin,emptyPassword);
+        loginPage.checkAuthEmptyError();
+
+    }
+    @Test
+    public void errorAuthTest2() {
+        LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+        loginPage.open();
+        loginPage.loginWithLoginButtonDefault(fakeLogin,fakePassword);
+        loginPage.checkAuthWrongError();
+
+    }
+
 }
--- src/test/java/ru/greenData/LoginPage.java
+++ src/test/java/ru/greenData/LoginPage.java
@@ -1,23 +1,86 @@
 package ru.greenData;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage {
-    WebDriver driver;
+    private WebDriver driver;
+    private WebDriverWait wait;
     public LoginPage(WebDriver driver){
         this.driver = driver;
+        wait = new WebDriverWait(driver, 10);
     }
 
+    private String errorMsgEmptyFields ="Аутентификация не удалась";
+    private String errorMsgWrongFields ="Неверное имя пользователя или пароль";
+
+    @FindBy(id = "login_button")
+    private WebElement loginButtonDefault;
+
+    @FindBy(id = "login_button_domain")
+    private  WebElement loginButtonDomain;
+
     @FindBy(id = "login_button_current")
-    private WebElement loginButton2;
+    private WebElement loginButtonCurrent;
+
+    @FindBy(id ="error")
+    private WebElement error;
+
+    @FindBy(id = "remember")
+    private WebElement remember;
+
+    @FindBy(id ="username")
+    private WebElement usernameField;
+
+    @FindBy(id= "password")
+    private WebElement passwordField;
+
+    private By errorMsgLocator = By.id("error");
+
 
     public void open() {
         driver.get("https://gdcloud.ru/release-17/auth/login");
     }
     public void getStarted() {
-        loginButton2.click();
+        loginButtonCurrent.click();
     }
+    private void authorization(String login, String password) {
+        usernameField.sendKeys(login);
+        passwordField.sendKeys(password);
+    }
+    public void loginWithLoginButtonDefault(String login, String password) {
+        authorization(login,password);
+        loginButtonDefault.click();
+
+    }
+    public void loginWithLoginButtonDomain(String login, String password) {
+        usernameField.sendKeys(login);
+        passwordField.sendKeys(password);
+        loginButtonDomain.click();
+    }
+    public void loginWithLoginButtonCurrent(String login, String password) {
+        usernameField.sendKeys(login);
+        passwordField.sendKeys(password);
+        loginButtonCurrent.click();
+    }
+    private void checkErrorMsg(String errorCheck){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(errorMsgLocator));
+        String emptyFieldsError= error.getText();
+        Assert.assertEquals(emptyFieldsError,errorCheck);
+        error.isEnabled();
+        error.isDisplayed();
+    }
+
+    public void checkAuthEmptyError() {
+        checkErrorMsg(errorMsgEmptyFields);
+    }
+    public void checkAuthWrongError() {
+        checkErrorMsg(errorMsgWrongFields);
+    }
+
 }
