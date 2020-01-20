32c3838de7c0db9c2125b9f2632a592a64fe8834
Жикин Сергей <sergei.zhikin@domru.ru>
2020-01-20 18:44:29 +0500

    version works before testng

---
+++ src/test/java/ru/greenData/AuthorizedPage.java
@@ -0,0 +1,29 @@
+package ru.greenData;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AuthorizedPage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+    public AuthorizedPage(WebDriver driver){
+        this.driver = driver;
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    private By feedLocator=By.className("feed-stream");
+
+    private String headerString = "Лента - WorkFlow";
+
+    private void checkLoginPageHeader() {
+        String title = driver.getTitle();
+        Assert.assertEquals(headerString, title);
+    }
+    public void checkSuccessfulAuth() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(feedLocator));
+        checkLoginPageHeader();
+    }
+}
--- src/test/java/ru/greenData/FirstTest.java
+++ src/test/java/ru/greenData/FirstTest.java
@@ -1,5 +1,7 @@
 package ru.greenData;
 
+import com.sun.org.glassfish.gmbal.Description;
+import com.sun.org.glassfish.gmbal.NameValue;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -13,6 +15,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class FirstTest extends WebDriverSettings {
     @Test
@@ -37,6 +40,14 @@ public class FirstTest extends WebDriverSettings {
         loginPage.loginWithLoginButtonDefault(fakeLogin,fakePassword);
         loginPage.checkAuthWrongError();
 
+    }
+    @Test
+    public void successLogin() {
+        LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+        loginPage.open();
+        loginPage.loginWithLoginButtonDefault(login,password);
+        AuthorizedPage authorizedPage = PageFactory.initElements(driver, AuthorizedPage.class);
+        authorizedPage.checkSuccessfulAuth();
     }
 
 }
