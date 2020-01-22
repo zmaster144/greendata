890345ab9af194be686eee138aefe04aecabf146
Жикин Сергей <sergei.zhikin@domru.ru>
2020-01-22 09:49:15 +0500

    auth cases

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -21,6 +21,10 @@ public class LoginPage {
     private String headerString = "WorkFlow";
     private String textLoginField ="Номер телефона или E-mail";
     private String textPasswordField="Пароль";
+    private String loginCorpText="Корпоративная соцсеть";
+    private String loginButtonDefaultText="Войти";
+    private String loginButtonDomainText="Войти (другая учетная запись)";
+    private String loginButtonCurrentText="Войти (текущая учетная запись)";
 
     @FindBy(id = "login_button")
     private WebElement loginButtonDefault;
@@ -44,9 +48,16 @@ public class LoginPage {
     private WebElement passwordField;
 
     private By errorMsgLocator = By.id("error");
+    private By loginLogo = By.className("login-logo");
+    private By loginCorp = By.xpath("//div[@class='login_corp'][contains(text(), 'Корпоративная соцсеть')]");
+    private By rememberMeTextLocator = By.xpath("//label[contains(text(), 'Запомнить меня')]");
+    private By rememberMeCheckBoxLocator = By.xpath("//div[@class='checkbox first']");
+    private By loginDefXpath = By.xpath("//button[@id='login_button'][contains(text(), 'Войти')]");
+    private By loginDomainXpath = By.xpath("//button[@id='login_button_domain'][contains(text(), 'Войти (другая учетная запись)')]");
+    private By loginCurrentXpath = By.xpath("//button[@id='login_button_current'][contains(text(), 'Войти (текущая учетная запись)')]");
 
 
-    public void open() {
+    public void openAuthPage() {
         driver.get("https://gdcloud.ru/release-17/auth/login");
     }
     private void authorization(String login, String password) {
@@ -86,9 +97,17 @@ public class LoginPage {
         String title = driver.getTitle();
         Assert.assertEquals(headerString, title);
     }
+
     public void checkLoginPageElements() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(loginLogo));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(loginCorp));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(rememberMeTextLocator));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(rememberMeCheckBoxLocator));
         Assert.assertEquals(usernameField.getAttribute("placeholder"),textLoginField);
         Assert.assertEquals(passwordField.getAttribute("placeholder"),textPasswordField);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(loginDefXpath));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(loginCurrentXpath));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(loginDomainXpath));
 
     }
 
--- src/test/java/ru/greenData/TestSuite.java
+++ src/test/java/ru/greenData/TestSuite.java
@@ -12,38 +12,58 @@ import utils.TestListener;
 
 @Listeners({ TestListener.class })
 public class TestSuite extends WebDriverSettings {
-    @Test
-    public void firstTest() {
-        LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
-        loginPage.open();
-        loginPage.checkLoginPageHeader();
-        loginPage.checkLoginPageElements();
-    }
-    @Test
-    public void errorAuthTest() {
-        LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
-        loginPage.open();
-        loginPage.loginWithLoginButtonCurrent(EMPTY_LOGIN, EMPTY_PASSWORD);
-        loginPage.checkAuthEmptyError();
 
-    }
+    @Description("Тест для проверки скриншота")
     @Test
-    public void errorAuthTest2() {
-        LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
-        loginPage.open();
-        loginPage.loginWithLoginButtonDefault(FAKE_LOGIN, FAKE_PASSWORD);
-        loginPage.checkAuthWrongError();
-
-    }
-    @Description
     @Severity(SeverityLevel.CRITICAL)
-    @Test
-    public void successLogin() {
+    public void a_makeScreenshot() {
         LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
-        loginPage.open();
-        loginPage.loginWithLoginButtonDefault(LOGIN, PASSWORD);
+        loginPage.openAuthPage();
+        loginPage.loginWithLoginButtonDefault(FAKE_LOGIN, FAKE_PASSWORD);
         AuthorizedPage authorizedPage = PageFactory.initElements(driver, AuthorizedPage.class);
+        //здесь он упадёт
         authorizedPage.checkSuccessfulAuth();
     }
 
+    @Description("Проверка элементов страницы авторизации")
+    @Test
+    @Severity(SeverityLevel.MINOR)
+    public void b_inspectLoginPageElements() {
+        LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+        loginPage.openAuthPage();
+        loginPage.checkLoginPageElements();
+    }
+
+    @Description("Проверка авторизации по кнопке Войти с пустыми полями")
+    @Test
+    @Severity(SeverityLevel.BLOCKER)
+    public void c_authDefaultWithEmptyFields1() {
+        LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+        loginPage.openAuthPage();
+        loginPage.loginWithLoginButtonDefault(EMPTY_LOGIN, EMPTY_PASSWORD);
+        //здесь он упадёт т.к. поле логин required и нажатие на кнопку с этими полями не бросает ошибок
+        loginPage.checkAuthEmptyError();
+    }
+
+    @Description("Проверка авторизации по кнопке Войти с пустыми логином и заполненым паролем")
+    @Test
+    @Severity(SeverityLevel.BLOCKER)
+    public void d_authDefaultWithEmptyFields2() {
+        LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+        loginPage.openAuthPage();
+        loginPage.loginWithLoginButtonDefault(EMPTY_LOGIN, PASSWORD);
+        //здесь он упадёт т.к. поле логин required и нажатие на кнопку с этими полями не бросает ошибок
+        loginPage.checkAuthEmptyError();
+    }
+
+    @Description("Проверка авторизации по кнопке Войти с пустыми логином и заполненым паролем")
+    @Test
+    @Severity(SeverityLevel.BLOCKER)
+    public void e_authDefaultWithEmptyFields3() {
+        LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+        loginPage.openAuthPage();
+        loginPage.loginWithLoginButtonDefault(LOGIN, EMPTY_PASSWORD);
+        //здесь он упадёт т.к. поле логин required и нажатие на кнопку с этими полями не бросает ошибок
+        loginPage.checkAuthEmptyError();
+    }
 }
